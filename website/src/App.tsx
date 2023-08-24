4971987284031241c04b454b3b8baa93e311d250
Enaium <wyy-666@hotmail.com>
2023-08-24 13:14:20 +0800

    前端初始化

---
+++ website/src/App.tsx
@@ -0,0 +1,8 @@
+import { defineComponent } from 'vue'
+import { RouterView } from 'vue-router'
+
+const App = defineComponent(() => {
+  return () => <RouterView />
+})
+
+export default App
---
+++ website/src/layouts/MainLayout.tsx
@@ -0,0 +1,7 @@
+import { defineComponent } from 'vue'
+
+const MainLayout = defineComponent(() => {
+  return () => <div>MainLayout</div>
+})
+
+export default MainLayout
